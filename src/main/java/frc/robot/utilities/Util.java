b8d26897b5932dfd5d8e511704ca57546b56a5ae
swirl <swurl@swurl.xyz>
2022-09-30 14:24:10 -0400

    remove extraneous util functions
    Signed-off-by: swirl <swurl@swurl.xyz>

--- src/main/java/frc/robot/utilities/Util.java
+++ src/main/java/frc/robot/utilities/Util.java
@@ -4,84 +4,9 @@
 
 package frc.robot.utilities;
 
-import edu.wpi.first.math.controller.RamseteController;
-import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj2.command.RamseteCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.utilities.drive.BeakDifferentialDrivetrain;
-
 /** Useful utility functions for drive. */
 public final class Util {
     public static final double speedScale(double input, double base, double throttle) {
         return input * (base + throttle * (1.0 - base));
-    }
-
-    /**
-     * Gets a command to control the
-     * drivetrain to follow a path.
-     * 
-     * @param traj Trajectory to follow.
-     * @param drivetrain
-     * @return A {@link SequentialCommandGroup} to run the trajectory, and stop the drivetrain.
-     * 
-     * @deprecated Drivetrains now implement this independently.
-     */
-    @Deprecated(forRemoval = true)
-    public static final SequentialCommandGroup getTrajectoryCommand(Trajectory traj,
-            BeakDifferentialDrivetrain drivetrain) {
-        return new RamseteCommand(
-                traj,
-                drivetrain::getPoseMeters,
-                new RamseteController(),
-                drivetrain.getFeedforward(),
-                drivetrain.getKinematics(),
-                drivetrain::getWheelSpeeds,
-                drivetrain.getDriveController(),
-                drivetrain.getDriveController(),
-                drivetrain::driveVolts,
-                drivetrain).andThen(() -> drivetrain.drive(0, 0, 0));
-    }
-
-    /**
-     * Convert native units of an encoder
-     * into a distance in inches travelled
-     * by a wheel.
-     * 
-     * @param nu            Native units to convert.
-     * @param cpr           Encoder ticks per motor revolution.
-     * @param gearRatio     Motor rotations per gearbox shaft rotation.
-     * @param wheelDiameter Diameter of the wheel, in inches.
-     * 
-     * @return <p>Wheel travel in meters.
-     *         </p>
-     * 
-     *         Example:
-     *         TalonSRX reports a velocity of 8192 NU/100ms, with a 10.75:1 gear
-     *         ratio, on a 4-inch wheel.
-     * 
-     *         <pre>
-     * {@code
-     * double travelMeters = Util.NUtoMeters(8192, 4096, 10.75, 4)
-     * }
-     * </pre>
-     * 
-     * <p>- 2 rotations of the motor (8192NU travel / 4096NU CPR)
-     * </p>
-     * <p>- ~.186 rotations of the wheel (2 motor rotations / 10.75 gear ratio)
-     * </p>
-     * <p>- ~2.34 in travel (.186 shaft rotations * (PI * 4 in))
-     * 
-     * @deprecated Due to the addition of <code>setDistancePerPulse</code> on {@link BeakMotorController},
-     * this function is no longer needed.
-     */
-    @Deprecated(forRemoval = true)
-    public static double NUtoMeters(double nu, double cpr, double gearRatio, double wheelDiameter) {
-        double motorRotations = (double) nu / cpr; // How many rotations of the motor in this amount of NU.
-        double wheelRotations = motorRotations / gearRatio; // How many rotations of the gearbox (wheel).
-        double positionMeters = wheelRotations * (Math.PI * Units.inchesToMeters(wheelDiameter)); // The distance
-                                                                                                  // travelled by the
-                                                                                                  // wheel.
-        return positionMeters;
     }
 }
